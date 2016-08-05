761149b876eadaf53ad18554d1f73c7262bc057b
Nick Stong <ns3116@cumc.columbia.edu>
2016-08-05 15:12:33 -0400

    Allow for different output directory, and missing bam locations

--- makeIGVbatch.sh
+++ makeIGVbatch.sh
@@ -11,27 +11,32 @@ while read line; do
         $(mysql --defaults-group-suffix=seqdb --defaults-file=/nfs/goldstein/software/annodb/.my.cnf -e "select AlignSeqFileLoc from seqdbClone where CHGVID = '$line' ;" |head -n2|tail -n1) \
         -name '*final.bam')
 
-    if [ -z "$tmp" ]
+    if [ -z "${tmp//\/\//\/}" ]
         then >&2 echo "ERROR: Did not find BAM for $line"
     elif [ $(du -sh $tmp|wc -l) -gt 1 ]
         then >&2 echo "ERROR: Found more than one BAM for $line"
              >&2 echo "$tmp"
     else
-        echo "$(readlink $tmp)"
+        echo "$(dirname $tmp)/$(basename $(readlink $tmp))"
     fi
 
 done<$(dirname $IN)/$(basename ${IN%.*}).list > $(dirname $IN)/$(basename ${IN%.*}).bamloc
 
-sed 's/\/nfs/\\\\10.73.52.21/' $(dirname $IN)/$(basename ${IN%.*}).bamloc |sed 's/10.73.52.21\/fastq/igm-avere.igm.cumc.columbia.edu\/fastq/' | sed 's/\//\\/g' > $(dirname $IN)/$(basename ${IN%.*}).bamwinloc
+#sed 's/\/nfs/\\\\10.73.52.21/' $(dirname $IN)/$(basename ${IN%.*}).bamloc |sed 's/\//\\/g' > $(dirname $IN)/$(basename ${IN%.*}).bamwinloc
+sed 's/\/nfs/\\\\10.73.52.21/' $(dirname $IN)/$(basename ${IN%.*}).bamloc |sed 's/10.73.52.21\/fastq\([0-9]\+\)/igm-avere.igm.cumc.columbia.edu\/fastq\1/' | sed 's/\//\\/g' > $(dirname $IN)/$(basename ${IN%.*}).bamwinloc
 
 while read i
-    do PRO_LOC=$(grep $(echo $i|awk '{print $2}') $(dirname $IN)/$(basename ${IN%.*}).bamwinloc|head -n1)
-       DAD_LOC=$(grep $(echo $i|awk '{print $3}') $(dirname $IN)/$(basename ${IN%.*}).bamwinloc|head -n1)
-       MOM_LOC=$(grep $(echo $i|awk '{print $4}') $(dirname $IN)/$(basename ${IN%.*}).bamwinloc|head -n1)
+    do PRO_LOC=$(grep -w $(echo $i|awk '{print $2}') $(dirname $IN)/$(basename ${IN%.*}).bamwinloc|head -n1)
+       if [ "$PRO_LOC" = "" ] ;then PRO_LOC="NA";fi
+       DAD_LOC=$(grep -w $(echo $i|awk '{print $3}') $(dirname $IN)/$(basename ${IN%.*}).bamwinloc|head -n1)
+       if [ "$DAD_LOC" = "" ] ;then DAD_LOC="NA";fi
+       MOM_LOC=$(grep -w $(echo $i|awk '{print $4}') $(dirname $IN)/$(basename ${IN%.*}).bamwinloc|head -n1)
+       if [ "$MOM_LOC" = "" ] ;then MOM_LOC="NA";fi
        VAR_CHR=$(echo $i|awk '{print $6}')
        VAR_LOC=$(echo $i|awk '{print $7}')
        VAR_GENE=$(echo $i|awk '{print $9}')
        PRO=$(echo $i|awk '{print $2}')
        echo "$PRO_LOC $DAD_LOC $MOM_LOC $VAR_CHR $VAR_LOC $((VAR_LOC-40)) $((VAR_LOC+40)) $PRO $VAR_GENE";done < <(sed 's/ /_/g' $IN|sed 's/-/\t/'|sed 's/-/\t/'|sed '1d'|sort -k6,6n) > $(dirname $IN)/$(basename ${IN%.*}).info.txt
 
-       awk -v dir=$(pwd|sed 's/\/nfs/\\\\10.73.52.21/'|sed 's/\//\\\\/g') '{print "#"$8" "$4":"$5"\n""new\ngenome 1kg_v37\nload " $1"\nload " $2"\nload "$3"\nsnapshotDirectory \\"dir"\ngoto "$4":"$6"-"$7"\nsort position\nsnapshot "$8"."$9"."$4"-"$5".png\ncollapse\nsnapshot "$8"."$9"."$4"-"$5".collapsed.png\n"}' $(dirname $IN)/$(basename ${IN%.*}).info.txt >$(dirname $IN)/$(basename ${IN%.*}).batch
+       dir=$(pwd|sed 's/\/nfs/\\\\10.73.52.21/'|sed 's/home\/[^\/]*/10.73.52.21\/homes/'|sed 's/\//\\\\/g')
+       awk -v dir="$dir" '{print "#"$8" "$4":"$5"\n""new\ngenome 1kg_v37\nload " $1"\nload " $2"\nload "$3"\nsnapshotDirectory \\"dir"\\IGV\ngoto "$4":"$6"-"$7"\nsort position\nsnapshot "$8"."$9"."$4"-"$5".png\ncollapse\nsnapshot "$8"."$9"."$4"-"$5".collapsed.png\n"}' $(dirname $IN)/$(basename ${IN%.*}).info.txt >$(dirname $IN)/$(basename ${IN%.*}).batch
