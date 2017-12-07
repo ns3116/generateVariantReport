ddbec07b5a7971ebe8e6f2ce23f0c545f526a910
Nick Stong <ns3116@cumc.columbia.edu>
2017-12-07 14:38:34 -0500

    Update databases, and data store ips.  Fixed dragen check so bam is found if it is the final symlink or not

--- makeIGVbatch.sh
+++ makeIGVbatch.sh
@@ -7,31 +7,39 @@ IN=$1
 awk '{print $2"\n"$3"\n"$4}' <(sed '1d' $IN) |sort -u > $(dirname $IN)/$(basename ${IN%.*}).list
 
 while read line; do  
-    alignLoc=$(mysql --defaults-group-suffix=seqdb --defaults-file=/nfs/goldstein/software/Bioinformatics_Tools/generateVariantReport/.my.cnf -e "select AlignSeqFileLoc from seqdbClone where CHGVID = '$line' ;" |head -n2|tail -n1|sed 's/_temp[0-9]\?/18/')
+    alignLoc=$(mysql --defaults-group-suffix=seqdb --defaults-file=~/.my.cnf -e "select AlignSeqFileLoc from seqdbClone where CHGVID = '$line' ;" |head -n2|tail -n1|sed 's/_temp[0-9]\?/18/')
     if [[ $alignLoc == "NULL" ]]
         then tmp=$(find \
-        $(mysql --defaults-group-suffix=seqdb --defaults-file=/nfs/goldstein/software/Bioinformatics_Tools/generateVariantReport/.my.cnf -e "select AlignSeqFileLoc from dragen_qc_metrics where CHGVID = '$line' ;" |head -n2|tail -n1|sed 's/_temp[0-9]\?/18/')/$line* \
+        $(mysql --defaults-group-suffix=seqdb --defaults-file=/nfs/goldstein/software/Bioinformatics_Tools/generateVariantReport/.my.cnf -e "select AlignSeqFileLoc from dragen_qc_metrics inner join dragen_sample_metadata on dragen_sample_metadata.pseudo_prepid = dragen_qc_metrics.pseudo_prepid where dragen_sample_metadata.sample_name = '$line' ;" |head -n2|tail -n1|sed 's/_temp[0-9]\?/18/')/$line* \
         -name '*recal.bam')
-    else 
+    else
         tmp=$(find \
         $alignLoc \
-        -name '*final.bam')
+        -name '*final.bam') #final
     fi
 
     if [ -z "${tmp//\/\//\/}" ]
-        then >&2 echo "ERROR: Did not find BAM for $line"
+        then 
+            tmp=$(find $alignLoc -name '*.bam')
+            if [ -z "${tmp//\/\//\/}" ]
+            then >&2 echo "ERROR: Did not find BAM for $line"
+            continue
+            fi
     elif [ $(du -sh $tmp|wc -l) -gt 1 ]
         then >&2 echo "ERROR: Found more than one BAM for $line"
              >&2 echo "$tmp"
-    else
-        if [ $alignLoc == "NULL" ];then echo $tmp
-        else echo "$(dirname $tmp)/$(basename $(readlink $tmp))";fi
-    fi
+             continue
+             fi
+    
+    if [ $alignLoc == "NULL" ];then echo $tmp
+    elif [[ -f $tmp ]];then echo $tmp
+    else echo "$(dirname $tmp)/$(basename $(readlink $tmp))";fi
+    
 
 done<$(dirname $IN)/$(basename ${IN%.*}).list|sort -u > $(dirname $IN)/$(basename ${IN%.*}).bamloc
 
-#sed 's/\/nfs/\\\\10.73.52.21/' $(dirname $IN)/$(basename ${IN%.*}).bamloc |sed 's/\//\\/g' > $(dirname $IN)/$(basename ${IN%.*}).bamwinloc
-sed 's/\/nfs/\\\\10.73.52.21/' $(dirname $IN)/$(basename ${IN%.*}).bamloc |sed 's/homes\/svaprojects/homes/'|sed 's/10.73.52.21\/fastq\([0-9]\+\)/igm-avere.igm.cumc.columbia.edu\/fastq\1/' | sed 's/\//\\/g'|sed 's/\\\\ALIGNMENT/\\ALIGNMENT/g' > $(dirname $IN)/$(basename ${IN%.*}).bamwinloc
+#sed 's/\/nfs/\\\\10.73.50.80/' $(dirname $IN)/$(basename ${IN%.*}).bamloc |sed 's/\//\\/g' > $(dirname $IN)/$(basename ${IN%.*}).bamwinloc
+sed 's/\/nfs/\\\\10.73.50.80/' $(dirname $IN)/$(basename ${IN%.*}).bamloc |sed 's/homes\/svaprojects/homes/'|sed 's/10.73.50.80\/fastq\([0-9]\+\)/igm-avere.igm.cumc.columbia.edu\/fastq\1/' | sed 's/\//\\/g'|sed 's/\\\\ALIGNMENT/\\ALIGNMENT/g' > $(dirname $IN)/$(basename ${IN%.*}).bamwinloc
 #sed 's/\/nfs/\\\\igm-avere.igm.cumc.columbia.edu/' $(dirname $IN)/$(basename ${IN%.*}).bamloc |sed 's/homes\/svaprojects/homes/' | sed 's/\//\\/g' > $(dirname $IN)/$(basename ${IN%.*}).bamwinloc
 
 while read i
@@ -47,5 +55,5 @@ while read i
        PRO=$(echo $i|awk '{print $2}')
        echo "$PRO_LOC $DAD_LOC $MOM_LOC $VAR_CHR $VAR_LOC $((VAR_LOC-40)) $((VAR_LOC+40)) $PRO $VAR_GENE";done < <(sed 's/ /_/g' $IN|sed 's/-/\t/'|sed 's/-/\t/'|sed '1d'|sort -k6,6n) > $(dirname $IN)/$(basename ${IN%.*}).info.txt
 
-       dir=$(pwd|sed 's/\/nfs/\\\\10.73.52.21/'|sed 's/home\/[^\/]*/10.73.52.21\/homes/'|sed 's/homes\/goldsteinlab/goldsteinlab/'|sed 's/homes\/svaprojects/svaprojects\/ns3116/'|sed 's/\//\\\\/g')
+       dir=$(pwd|sed 's/\/nfs/\\\\10.73.50.80/'|sed 's/home\/[^\/]*/10.73.50.80\/homes/'|sed 's/homes\/goldsteinlab/goldsteinlab/'|sed 's/homes\/svaprojects/svaprojects\/ns3116/'|sed 's/\//\\\\/g')
        awk -v dir="$dir" '{print "#"$8" "$4":"$5"\n""new\ngenome 1kg_v37\nload " $1"\nload " $2"\nload "$3"\nsnapshotDirectory \\"dir"\\IGV\ngoto "$4":"$6"-"$7"\nsort position\ncolor read strand\nsnapshot "$9"."$8"."$4"-"$5".png\ncollapse\nsnapshot "$9"."$8"."$4"-"$5".collapsed.png\n"}' $(dirname $IN)/$(basename ${IN%.*}).info.txt >$(dirname $IN)/$(basename ${IN%.*}).batch
